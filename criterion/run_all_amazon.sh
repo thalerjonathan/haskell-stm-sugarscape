868a6287aea404fa2181c40687eea7688eb03a93
Jonathan Thaler <thaler.jonathan@gmail.com>
2019-12-09 22:08:44 +0100

    ignoring sugarscape io on amazon, takes too long, costs too much and does not really add anything new

--- criterion/run_all_amazon.sh
+++ criterion/run_all_amazon.sh
@@ -1,7 +1,3 @@
-cd sugarscape-io/
-sh run_amazon.sh
-cd ..
-
 cd sugarscape-tarray/
 sh run_amazon.sh
 cd ..
--- criterion/sugarscape-tarray/run_amazon.sh
+++ criterion/sugarscape-tarray/run_amazon.sh
@@ -2,6 +2,10 @@ stack exec -- sugarscape-tarray sugarscape-tarray-normal -o sugarscape-tarray-no
 
 stack exec -- sugarscape-tarray sugarscape-tarray-normal -o sugarscape-tarray-normal_amazon_500_32.html +RTS -N32
 
+stack exec -- sugarscape-tarray sugarscape-tarray-normal -o sugarscape-tarray-normal_amazon_500_64.html +RTS -N64
+
 stack exec -- sugarscape-tarray sugarscape-tarray-rebirthing:16/2500 -o sugarscape-tarray-rebirthing_amazon_2500_16.html +RTS -N16
 
 stack exec -- sugarscape-tarray sugarscape-tarray-rebirthing:32/2500 -o sugarscape-tarray-rebirthing_amazon_2500_32.html +RTS -N32
+
+stack exec -- sugarscape-tarray sugarscape-tarray-rebirthing:64/2500 -o sugarscape-tarray-rebirthing_amazon_2500_64.html +RTS -N64
